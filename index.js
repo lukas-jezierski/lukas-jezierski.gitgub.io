cad9f69326f60a3be2154b6e2200e390e189fac7
lukas-jezierski <54543782+lukas-jezierski@users.noreply.github.com>
2023-04-17 16:30:33 +0200

    Create index.js
    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const button = document.getElementById('btfetch');
+const outputDiv = document.getElementById('output');
+
+
+button.addEventListener('click', () => {
+    fetch('https://countries.trevorblades.com/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            countries {
+              code
+              name
+            }
+          }
+        `,
+      }),
+    })
+      .then(res => res.json())
+      .then(({ data }) => {
+        const countries = data.countries;
+        const countryList = countries.map(country => `<li>${country.name} (${country.code})</li>`).join('');
+        const outputHTML = `<ul>${countryList}</ul>`;
+        outputDiv.innerHTML = outputHTML;
+      });
+  });
